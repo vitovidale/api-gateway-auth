579a1c513d4efed622fb487db0c8a118f86fd511
Rodrigo Longhi <rolonghi@pm.me>
2025-03-12 23:25:52 -0300

    feat: minor changes to support API Gateway proxy events

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -11,8 +12,14 @@ type MyEvent struct {
 	Name string `json:"name"`
 }
 
-func HandleRequest(ctx context.Context, event MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", event.Name), nil
+func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	name := req.QueryStringParameters["name"]
+	message := fmt.Sprintf("Hello %s!", name)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       message,
+	}, nil
 }
 
 func main() {
